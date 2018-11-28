df13dc4d6a194872bb2641a5db14691050614324
Clément Jumel <clement.jumel@polytechnique.edu>
2018-11-28 09:22:59 -0500

    add the trained models

--- histograms.py
+++ histograms.py
@@ -1,38 +1,36 @@
 from classifier import *
 from features import load_features
-import seaborn as sns
+
 import numpy as np
-
-# Parameters
-classifier = 'LSTM'
-method = 'we'
-language = 'en'
-
-# Load the file
-load_file = 'data/model/trained_' + classifier + '_' + method + '_' + language
-model = load_classifier(classifier, load_file)
-
-# Load the data
-x_train, x_test, y_train, y_test = load_features(language, method)
-
-# Predict on the sets
-y_pred_train = predict_classifier(model, x_train)
-y_pred_test = predict_classifier(model, x_test)
-
-# Compute the performance
-print("RMSE (train): {}".format(evaluate_classifier("RMSE", y_train, y_pred_train)))
-print("Accuracy (train): {}".format(evaluate_classifier("multi_accuracy", y_train, y_pred_train)))
-print("Binary accuracy (train): {}".format(evaluate_classifier("binary_accuracy", y_train, y_pred_train)))
-print("RMSE (test): {}".format(evaluate_classifier("RMSE", y_test, y_pred_test)))
-print("Accuracy (test): {}".format(evaluate_classifier("multi_accuracy", y_test, y_pred_test)))
-print("Binary accuracy (test): {}".format(evaluate_classifier("binary_accuracy", y_test, y_pred_test)))
-
-# Compute the histograms
 import matplotlib.pyplot as plt
-y_pred = np.concatenate((y_pred_train, y_pred_test), axis=0)
+import seaborn as sns
+
+
+language = 'fr'
+
+# Compute the true values
+_, _, y_train, y_test = load_features(language, 'bow')
 y_true = np.concatenate((y_train, y_test), axis=0)
-sns.distplot(y_pred)
+
+classifiers = ['SVM', 'NN', 'LSTM']
+methods = ['bow', 'bow', 'we']
+y_pred = []
+
+for i in range(3):
+    print('')
+    print("{}/3...".format(i+1))
+    method = methods[i]
+    classifier = classifiers[i]
+    x_train, x_test, _, _ = load_features(language, method)
+    load_file = 'data/model/trained_' + classifier + '_' + method + '_' + language
+    model = load_classifier(classifier, load_file)
+    y_pred_train = predict_classifier(model, x_train)
+    y_pred_test = predict_classifier(model, x_test)
+    y_pred.append(np.concatenate((y_pred_train, y_pred_test), axis=0))
+
+sns.distplot(y_true, hist=False, kde_kws={"color": "r", "lw": 3, "label": "True"})
+for i in range(1, 3):
+    y_pred = np.load('data/saved_distributions/distributions_'+language+'_'+str(i)+'.npy')
+    sns.distplot(y_pred, hist=False, kde_kws={"label": classifiers[i]})
 plt.show()
 
-sns.distplot(y_true)
-plt.show()
